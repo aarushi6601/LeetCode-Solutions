9ea0deb4a82e110863e43e58c1abc8a68fe5f941
Aarushi Agarwal <71183078+aarushi6601@users.noreply.github.com>
2023-06-20 22:27:09 +0530

    Time: 7 ms (90.85%), Space: 9.8 MB (89.47%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        set<int>s;
-for(int i=0;i<nums.size();i++)
-{
-    if(s.count(nums[i])==0)
-        s.insert(nums[i]);
-    else s.erase(nums[i]);
-}
-        return {s.begin(),s.end()};
+        long long int xy=0;
+        for(int n:nums) xy ^=n;
+        xy &= -xy;
+        vector<int>result(2,0);
+        for(int n:nums)
+        {
+            if((n&xy)==0)
+                result[0]^=n;
+            else 
+                result[1]^=n;
+            
+        }
+        return result;
     }
 };
