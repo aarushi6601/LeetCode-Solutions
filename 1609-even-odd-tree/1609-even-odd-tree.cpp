be5e611ed937f94dd3e8fc12fc7d26d40ceff162
Aarushi Agarwal <71183078+aarushi6601@users.noreply.github.com>
2023-07-08 11:05:53 +0530

    Time: 304 ms (54.95%), Space: 150.7 MB (98.22%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.cpp
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -12,33 +12,26 @@
 class Solution {
 public:
     bool isEvenOddTree(TreeNode* root) {
-      if(root==NULL) return true;
-        queue<TreeNode*>q;
+        queue<TreeNode*> q;
         q.push(root);
-        int level=0;
-        while(!q.empty()){
-            int size=q.size();
-            int prev=level%2==0?0:INT_MAX;
-            for(int i=0;i<size;i++)//this loop is for going through each level
-            {
-                TreeNode* node=q.front();
-                if(level%2==0)
-                {
-                    if(node->val%2==0 || node->val<=prev) return false;
-                }
-                else
-                {
-                    if(node->val%2|| node->val>=prev) return false;
-                }
-                prev=node->val;
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
+        int lv = 0;
+        while (q.size()) {
+            int cnt = q.size(), prev = lv % 2 == 0 ? 0 : INT_MAX;
+            while (cnt--) {
+                auto node = q.front();
                 q.pop();
+                if (lv % 2 == 0) {
+                    if (node->val % 2 == 0 || node->val <= prev) return false;
+                } else {
+                    if (node->val % 2 || node->val >= prev) return false;
+                }
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+                                prev = node->val;
+
             }
-            level++;
+            ++lv;
         }
         return true;
-        }
-        
-    
+    }
 };
