93b85c2286bab78a13f66584c0ddc9e74559783f
Aarushi Agarwal <71183078+aarushi6601@users.noreply.github.com>
2023-09-10 17:53:56 +0530

    Time: 12 ms (22.19%), Space: 15.2 MB (65.63%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        string res=s;
-        for(int i=0;i<indices.size();i++)
-        { res[indices[i]]=s[i];
-        }  return res;
+    //     string res=s;
+    //     for(int i=0;i<indices.size();i++)
+    //     { res[indices[i]]=s[i];
+    //     }  return res;
+    // }
+
+//cycling sort using O(1) space
+for(int i=0;i<indices.size();i++)
+{
+    while(indices[i]!=i)
+    {
+        swap(s[i],s[indices[i]]);
+        swap(indices[i],indices[indices[i]]);
+    }
+}
+return s;
     }
 };
