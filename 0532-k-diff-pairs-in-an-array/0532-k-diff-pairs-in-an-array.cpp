3b9a5cf4e23cd3569a0363a9de2d298aa1754231
Aarushi Agarwal <71183078+aarushi6601@users.noreply.github.com>
2023-09-29 15:41:24 +0530

    Time: 19 ms (35.86%), Space: 14.4 MB (14.69%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        int res=0;
-        for(auto i:nums) mp[i]++;
-        for(auto i:mp)
-            if( (!k && i.second>1) || (k && mp.find(i.first+k)!=mp.end()) ) res++;
-        return res;
+        //can use set but just b sure of k=0 ie if the set has repeated elements what to do
+        set<int>s;
+        set<int>repeated;
+        for(int x:nums)
+        {
+            if(s.count(x))
+                repeated.insert(x);
+            s.insert(x);
+        }
+        if(k==0) return repeated.size();
+        int ans=0;
+        for(int x:s)
+        {
+            if(s.find(x+k)!=s.end())
+                ans++;
+        }
+        return ans;
     }
 };
