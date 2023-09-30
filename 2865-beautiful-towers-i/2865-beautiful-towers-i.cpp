b49561123d260f1ed06429751c3bd1350554cb51
Aarushi Agarwal <71183078+aarushi6601@users.noreply.github.com>
2023-10-01 02:19:04 +0530

    Time: 59 ms (55.32%), Space: 24.2 MB (58.64%) - LeetHub

---
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maximumSumOfHeights(vector<int>& maxHeights) {
+    long long ans = 0;
+    for(int i = 0; i < maxHeights.size(); ++i){
+        long long sum =  maxHeights[i], last = maxHeights[i];
+
+        // Calculate sum of heights to the left
+        for(int j = i - 1; j >= 0; --j){ 
+            last = min((long long)maxHeights[j], last); 
+            sum += last; 
+        }
+
+        last = maxHeights[i];
+        // Calculate sum of heights to the right
+        for(int j = i + 1; j < maxHeights.size(); ++j){ 
+            last = min((long long)maxHeights[j], last); 
+            sum += last; 
+        }
+        ans = max(ans, sum);
+    }
+    return ans;
+
+    }
+};
