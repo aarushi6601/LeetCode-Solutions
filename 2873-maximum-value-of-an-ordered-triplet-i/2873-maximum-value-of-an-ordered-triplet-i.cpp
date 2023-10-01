c62d347ffc5654ace09d4ecec9763d033c7ff811
Aarushi Agarwal <71183078+aarushi6601@users.noreply.github.com>
2023-10-01 21:17:39 +0530

    Time: 3 ms (92.31%), Space: 18.2 MB (7.69%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        //[ai-aj]*ak
-        //ans=total val, max1=ai-aj, max2=ai
+        int n=nums.size();
+        vector<int>prf(n,0),suff(n,0);
+        int m=nums[0];
+        for(int i=1;i<n;i++){
+            prf[i]=m;
+            m=max(m,nums[i]);
+        }
+        m=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            suff[i]=m;
+            m=max(m,nums[i]);
+        }
         long long ans=0;
-        int max1=0,max2=0;
-        for(int &x:nums)
-        {
-            ans=max(ans,1LL*max1*x);
-            max1=max(max1, max2-x);
-            max2=max(max2,x);
+        for(int i=0;i<n-1;i++){
+            long long v= (prf[i]-(1LL*nums[i]))*(1LL*suff[i]);
+           ans=max(ans,v);
         }
         return ans;
+        
     }
 };
+
+//to convrt an int to long w multiply it with 1LL
+// //It consists of three elements from the array.
+// The first element is the maximum value in the prefix of the current element.
+// The second element is the current element itself.
+// The third element is the maximum value in the suffix of the current element.
