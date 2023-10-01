5b717a354c0960b71efac6af1a9b8043b825faf4
Aarushi Agarwal <71183078+aarushi6601@users.noreply.github.com>
2023-10-01 21:25:50 +0530

    Time: 0 ms (100.00%), Space: 18.2 MB (7.69%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
@@ -4,12 +4,12 @@ public:
         vector<int> l(nums.size()), r(nums.size());
         // prefix max
         l[0] = nums[0];
-        for (int i = 1; i < nums.size() - 1; i++) {
+        for (int i = 1; i < nums.size() ; i++) {
             l[i] = max(l[i - 1], nums[i - 1]);
         }
         // suffix max
         r[nums.size() - 1] = nums[nums.size() - 1];
-        for (int i = nums.size() - 2; i >= 1; i--) {
+        for (int i = nums.size() - 2; i >= 0; i--) {
             r[i] = max(r[i + 1], nums[i + 1]);
         }
         // initialize ans as 0 to automatically eliminate any negative values
