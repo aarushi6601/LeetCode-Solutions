b861dcf5bd1c8ee560601394fde43ce737775f5f
Aarushi Agarwal <71183078+aarushi6601@users.noreply.github.com>
2023-10-01 23:06:55 +0530

    Time: 0 ms (100.00%), Space: 18.3 MB (7.69%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        vector<int> prf(nums.size()), suff(nums.size());
+        vector<int> l(nums.size()), r(nums.size());
         // prefix max
-        prf[0] = nums[0];
+        l[0] = nums[0];
         for (int i = 1; i < nums.size() ; i++) {
-            prf[i] = max(prf[i - 1], nums[i-1]);
+            l[i] = max(l[i - 1], nums[i - 1]);
         }
         // suffix max
-        suff[nums.size() - 1] = nums[nums.size() - 1];
+        r[nums.size() - 1] = nums[nums.size() - 1];
         for (int i = nums.size() - 2; i >= 0; i--) {
-            suff[i] = max(suff[i + 1], nums[i+1]);
+            r[i] = max(r[i + 1], nums[i + 1]);
         }
         // initialize ans as 0 to automatically eliminate any negative values
         long long ans = 0;
         for (int i = 1; i < nums.size() - 1; i++) {
-            ans = max(ans, (long long)(prf[i] - nums[i]) * suff[i]);
+            ans = max(ans, (long long)(l[i] - nums[i]) * r[i]);
         }
         return ans;
     }
