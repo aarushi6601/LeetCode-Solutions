1ed8d25e8ed3fe1765eed89bbc977084cefc71a2
Aarushi Agarwal <71183078+aarushi6601@users.noreply.github.com>
2023-10-06 01:33:02 +0530

    Time: 7 ms (89.81%), Space: 16 MB (19.46%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-         map<int,int>mp;
-        for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]=i+1;
-        }
+         //two pointrs approach for O(1) space as input array is also sortd
+        int lft=0,right=nums.size()-1;
         vector<int>ans;
-        for(int i=0;i<nums.size();i++)
+        while(lft<right)
         {
-            if(mp.find(target-nums[i])!=mp.end())
-            {     ans.push_back(i+1);
-                  ans.push_back(mp[target-nums[i]]);
-            break;
+            int sum=nums[lft]+nums[right];
+            if(sum>target)
+                right--;
+            else if(sum<target)
+                lft++;
+            else
+            {
+                ans.push_back(lft+1);
+                ans.push_back(right+1);
+                break;
             }
         }
         return ans;
